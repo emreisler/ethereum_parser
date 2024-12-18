04e9956026fb89bb2b4a9d2e2908e223fe77b5a1
emreisler <emre.isler.9@gmail.com>
2024-12-18 15:55:46 +0300

    Added some TODO' s to mention what can be improved

--- usecases/parser/eth_parser.go
+++ usecases/parser/eth_parser.go
@@ -39,6 +39,9 @@ func (p *ethParser) Subscribe(address string) bool {
 	return true
 }
 
+// GetTransactions return retrieved transactions belongs to given address
+// TODO It is not returning the transactions happened after last block update handling, so some new transactions are missing
+// but the purpose is notification so 5 second delay may be acceptable ?
 func (p *ethParser) GetTransactions(address string) []domain.Transaction {
 	if !p.subscriberRepo.SubscriberExists(address) {
 		return nil
@@ -65,18 +68,20 @@ func (p *ethParser) handleBlockUpdates(blocksChan <-chan int) {
 	}
 }
 
+// populateTxsWithBlockRange loop through from the current block to the latest retrieved block from eth
 func (p *ethParser) populateTxsWithBlockRange(startBlock, endBlock int) {
 	for block := startBlock; block <= endBlock; block++ {
 		p.populateTxs(block)
 	}
 }
 
+// populateTxs if current block
+// TODO name of the method does not say too much about the logic
 func (p *ethParser) populateTxs(block int) error {
 	if block == p.currentBlock {
 		return p.populateExistingBlock(block)
 	}
 	return p.populateNewBlock(block)
-
 }
 
 // addToObserver check the retrieved transaction belongs to any subscriber, if it is, add hash to the subscriber
@@ -92,6 +97,12 @@ func (p *ethParser) addToObserver(tx *domain.Transaction) {
 }
 
 // populateNewBlock fetch all transaction with details
+// it is a new block for our parser, so we need to fetch all transaction details
+// TODO(1) why I keep all the transactions in repo ? Bec, if there is no subscriber at this time keeping the tx is non-sense
+// or I should suppose to give subscriber txs from the beginning block ?
+// design error
+// I need to check if the transaction belongs to any subscriber, otherwise I should not keep it..
+// Another scenario is giving all the history from starting block no on , but is this the requirement ?
 func (p *ethParser) populateNewBlock(block int) error {
 	txs, err := p.ethClient.GetTxObjects(block)
 	if err != nil {
@@ -99,6 +110,7 @@ func (p *ethParser) populateNewBlock(block int) error {
 	}
 
 	for _, tx := range txs {
+		//I should have been checked if tx belongs to any subscriber..
 		if !p.txRepo.TxExist(tx.Hash) {
 			p.txRepo.AddTx(tx)
 		}
@@ -108,6 +120,10 @@ func (p *ethParser) populateNewBlock(block int) error {
 }
 
 // populateExistingBlock fetch just transaction hashes and retrieve complete transaction if the hash does not exist in storage
+// TODO starts with fetching just hashes bec, we are assuming we have most of the txs from this already scanned block
+// to be able to reduce the payload from eth node
+// looping through the hashes and fetch the tx details by using tx hash
+// downside is what if we couldn't retrieve most of the transactions so there will be too many requests one by one
 func (p *ethParser) populateExistingBlock(block int) error {
 	txHashes, err := p.ethClient.GetTxHashes(block)
 	if err != nil {
@@ -127,6 +143,8 @@ func (p *ethParser) populateExistingBlock(block int) error {
 	return nil
 }
 
+// watchLatestBlock watches latest block and return a channel of block number
+// TODO I may check if the retrieved current block is higher than existing block here ?
 func (p *ethParser) watchLatestBlock() <-chan int {
 	ticker := time.NewTicker(5 * time.Second)
 	blockChan := make(chan int)
